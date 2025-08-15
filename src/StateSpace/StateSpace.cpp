496f41e1efe3b2279ed8870aed0053167fd10a4e
CuinnKemp <cuinn.kemp@student.adelaide.edu.au>
2025-08-15 17:53:35 +0930

    fixing build, adding Model and testing

--- src/StateSpace/StateSpace.cpp
+++ src/StateSpace/StateSpace.cpp
@@ -1,58 +1,35 @@
-/* 
+#include "StateSpace.hpp"
 
-StateSpace.hpp
-Implements a StateSpace class that allows for interactions with an n-dimensional integer indexed array.
-
-*/
-
-
-#ifndef STATESPACE_H
-#define STATESPACE_H
-#include <vector>
-#include <iostream>
-#include <cmath>
-
-class StateSpace {
-private:
-    std::vector<double> stateSpace;
-    const int dimensions;
-    const int dimensionSize;
-
-    int coords_to_index(const std::vector<int>& coords) const {
-        if (coords.size() != this->dimensions) 
-            throw std::invalid_argument("Index has Invalid Number of Dimensions");
-        int rawIndex = 0;
-        int multiplier = 1;
-        for (int i = dimensions - 1; i > -1; i--) {
-            if (coords[i] < 0 || coords[i] >= this->dimensionSize) 
-                throw std::out_of_range("Coordinate at index [" + std::to_string(i) + "] out of range");
-            rawIndex += coords[i] * multiplier;
-            multiplier *= dimensionSize;
-        }
-        return rawIndex;
+int StateSpace::coords_to_index(const std::vector<int>& coords) const {
+    if (coords.size() != this->dimensions) 
+        throw std::invalid_argument("Index has Invalid Number of Dimensions");
+    int rawIndex = 0;
+    int multiplier = 1;
+    for (int i = dimensions - 1; i > -1; i--) {
+        if (coords[i] < 0 || coords[i] >= this->dimensionSize) 
+            throw std::out_of_range("Coordinate at index [" + std::to_string(i) + "] out of range");
+        rawIndex += coords[i] * multiplier;
+        multiplier *= dimensionSize;
     }
+    return rawIndex;
+}
 
-public:
-    StateSpace(int dimensions, int dimensionSize, double initialValues = 0.0) 
-        : dimensions(dimensions), dimensionSize(dimensionSize), stateSpace(std::pow(dimensionSize, dimensions), initialValues) {}
+StateSpace::StateSpace(int dimensions, int dimensionSize, double initialValues) 
+    : dimensions(dimensions), dimensionSize(dimensionSize), stateSpace(std::pow(dimensionSize, dimensions), initialValues) {}
 
 
-    int get_dimensions() const {
-        return this->dimensions;
-    }
+int StateSpace::get_dimensions() const {
+    return this->dimensions;
+}
 
-    int get_dimension_size() const {
-        return this->dimensionSize;
-    }
-    
-    double get(const std::vector<int>& coords) const {
-        return this->stateSpace[coords_to_index(coords)];
-    }
+int StateSpace::get_dimension_size() const {
+    return this->dimensionSize;
+}
 
-    double set(const std::vector<int>& coords, double value) {
-        return this->stateSpace[coords_to_index(coords)] = value;
-    }
-    
-};
+double StateSpace::get(const std::vector<int>& coords) const {
+    return this->stateSpace[coords_to_index(coords)];
+}
 
-#endif // STATESPACE_H
+double StateSpace::set(const std::vector<int>& coords, double value) {
+    return this->stateSpace[coords_to_index(coords)] = value;
+}
---
+++ src/StateSpace/StateSpace.hpp
@@ -0,0 +1,36 @@
+/* 
+
+StateSpace.hpp
+Implements a StateSpace class that allows for interactions with an n-dimensional integer indexed array.
+
+*/
+
+
+#ifndef STATESPACE_H
+#define STATESPACE_H
+#include <vector>
+#include <iostream>
+#include <cmath>
+
+class StateSpace {
+private:
+    std::vector<double> stateSpace;
+    const int dimensions;
+    const int dimensionSize;
+
+    int coords_to_index(const std::vector<int>& coords) const;
+
+public:
+    StateSpace(int dimensions, int dimensionSize, double initialValues = 0.0);
+
+    int get_dimensions() const;
+
+    int get_dimension_size() const;
+    
+    double get(const std::vector<int>& coords) const;
+
+    double set(const std::vector<int>& coords, double value);
+    
+};
+
+#endif // STATESPACE_H
