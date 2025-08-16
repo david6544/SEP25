ad706eeeda217005773ac18050c9aadf8cd821d8
David M <david.maslov@student.adelaide.edu.au>
2025-08-16 11:48:50 +0930

    chore: moved over from personal repo to the adelaide uni one

---
+++ src/Models/DumbModel.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <random>
+#include <ctime>
+
+#include "DumbModel.hpp"
+
+DumbModel::DumbModel(int dimensions, int dimensionSize) : Model(dimensions, dimensionSize) {
+    std::srand(std::time(nullptr));
+}
+
+std::vector<int> DumbModel::get_next_query() {
+    int D = this->stateSpace->get_dimensions();
+    int K = this->stateSpace->get_dimension_size();
+    std::vector<int> nextQuery(D, 0);
+
+    for (auto &var : nextQuery){
+        var = std::rand() % K;
+    }
+
+    return nextQuery;
+}
+
+void DumbModel::update_prediction(std::vector<int> query, double result) {
+    stateSpace->set(query, result);
+}
+
---
+++ src/StateSpace/StateSpace.cpp
@@ -0,0 +1,42 @@
+#include "StateSpace.hpp"
+
+int StateSpace::coords_to_index(const std::vector<int>& coords) const {
+    if (coords.size() != this->dimensions) 
+        throw std::invalid_argument("Index has Invalid Number of Dimensions");
+    int rawIndex = 0;
+    int multiplier = 1;
+    for (int i = dimensions - 1; i > -1; i--) {
+        if (coords[i] < 0 || coords[i] >= this->dimensionSize) 
+            throw std::out_of_range("Coordinate at index [" + std::to_string(i) + "] out of range");
+        rawIndex += coords[i] * multiplier;
+        multiplier *= dimensionSize;
+    }
+    return rawIndex;
+}
+
+StateSpace::StateSpace(int dimensions, int dimensionSize, double initialValues) 
+    :   dimensions(dimensions), 
+        dimensionSize(dimensionSize), 
+        stateSpace(std::pow(dimensionSize, dimensions), initialValues)
+    {}
+
+
+int StateSpace::get_dimensions() const {
+    return this->dimensions;
+}
+
+int StateSpace::get_dimension_size() const {
+    return this->dimensionSize;
+}
+
+double StateSpace::get(const std::vector<int>& coords) const {
+    return this->stateSpace[coords_to_index(coords)];
+}
+
+std::vector<double> StateSpace::get_raw_representation() const {
+    return this->stateSpace;
+}
+
+double StateSpace::set(const std::vector<int>& coords, double value) {
+    return this->stateSpace[coords_to_index(coords)] = value;
+}
