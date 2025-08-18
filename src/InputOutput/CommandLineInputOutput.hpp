501802acdf2221a029f030b437d01c4b43b99dde
Cuinn Glynn Kemp <cuinn.kemp@student.adelaide.edu.au>
2025-08-18 18:11:49 +0930

    adding IO and testing (#10)
    Co-authored-by: a1870737 <a1870737@adelaide.edu.au>

---
+++ src/InputOutput/CommandLineInputOutput.hpp
@@ -0,0 +1,15 @@
+#ifndef COMMAND_LINE_IO_H
+#define COMMAND_LINE_IO_H
+#include <vector>
+
+#include "InputOutput.hpp"
+
+class CommandLineInputOutput : public InputOutput {
+    CommandLineInputOutput() = default;
+public:
+    double send_query_recieve_result(std::vector<int> &query) override;
+    void output_state(StateSpace &stateSpace) override;
+    static void set_IO();
+};
+
+#endif // COMMAND_LINE_IO_H
---
+++ src/InputOutput/InputOutput.hpp
@@ -0,0 +1,17 @@
+#ifndef INPUT_OUTPUT_H
+#define INPUT_OUTPUT_H
+#include <vector>
+#include "../StateSpace/StateSpace.hpp"
+
+class InputOutput {
+protected:
+    static InputOutput* instance;
+    InputOutput();
+public:
+    virtual double send_query_recieve_result(std::vector<int> &query) = 0;
+    virtual void output_state(StateSpace &stateSpace) = 0;
+    
+    static InputOutput* get_instance();
+};
+
+#endif // INPUT_OUTPUT_H
