2681d9dae80c79bc065a3fed4cb36aaea6854757
a1870737 <a1870737@adelaide.edu.au>
2025-09-11 17:10:01 +0930

    cmake fixes

--- windows-toolchain.cmake
+++ windows-toolchain.cmake
@@ -1,15 +1,28 @@
 # windows-toolchain.cmake
 set(CMAKE_SYSTEM_NAME Windows)
 
-# set(CMAKE_C_COMPILER   /usr/bin/x86_64-w64-mingw32-gcc)
-# set(CMAKE_CXX_COMPILER /usr/bin/x86_64-w64-mingw32-g++)
+# Detect host system to select correct mingw-w64 path
+if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Darwin")
+    # macOS with Homebrew mingw-w64
+    set(MINGW_PREFIX /opt/homebrew/opt/mingw-w64/bin/x86_64-w64-mingw32)
+elseif(CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux")
+    # Linux with distro mingw-w64
+    set(MINGW_PREFIX /usr/bin/x86_64-w64-mingw32)
+else()
+    message(FATAL_ERROR "Unsupported host system for Windows cross-compilation: ${CMAKE_HOST_SYSTEM_NAME}")
+endif()
 
-# For wingw instaleld by homebrew on macOS
-set(CMAKE_C_COMPILER   /opt/homebrew/opt/mingw-w64/bin/x86_64-w64-mingw32-gcc)
-set(CMAKE_CXX_COMPILER /opt/homebrew/opt/mingw-w64/bin/x86_64-w64-mingw32-g++)
+set(CMAKE_C_COMPILER   ${MINGW_PREFIX}-gcc)
+set(CMAKE_CXX_COMPILER ${MINGW_PREFIX}-g++)
 
-set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
+# Root path (for libraries + headers)
+if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Darwin")
+    set(CMAKE_FIND_ROOT_PATH /opt/homebrew/opt/mingw-w64/x86_64-w64-mingw32)
+elseif(CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux")
+    set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
+endif()
 
+# Search rules for cross compilation
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
